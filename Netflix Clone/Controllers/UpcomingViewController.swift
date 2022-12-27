dc6bd96998b65b6efdcdfb9c35a54382ef04dfa8
burak <burakaydin9761@gmail.com>
2022-12-27 19:53:10 +0300

    Creating custom TableViewCell from the upcoming table

--- Netflix Clone/Controllers/UpcomingViewController.swift
+++ Netflix Clone/Controllers/UpcomingViewController.swift
@@ -9,8 +9,58 @@ import UIKit
 
 class UpcomingViewController: UIViewController {
 
+    private let upcomingTable: UITableView = {
+        let table = UITableView()
+        table.register(UITableViewCell.self, forCellReuseIdentifier: "cell")
+        return table
+    }()
+    
+    private var movies: [Movie] = []
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         view.backgroundColor = .systemBackground
+        title = "Upcoming"
+        navigationController?.navigationBar.prefersLargeTitles = true
+        navigationController?.navigationItem.largeTitleDisplayMode = .always
+
+        view.addSubview(upcomingTable)
+        upcomingTable.delegate = self
+        upcomingTable.dataSource = self
+        
+        fetchUpcoming()
     }
+    
+    override func viewDidLayoutSubviews() {
+        super.viewDidLayoutSubviews()
+        upcomingTable.frame = view.bounds
+    }
+    
+    private func fetchUpcoming(){
+        APICaller.shared.getUpcomingMovies { [weak self] result in
+            switch result {
+            case .success(let movies):
+                self?.movies = movies
+                DispatchQueue.main.async {
+                    self?.upcomingTable.reloadData()
+                }
+            case .failure(let error):
+                print(error.localizedDescription)
+            }
+        }
+    }
+}
+
+extension UpcomingViewController: UITableViewDelegate, UITableViewDataSource{
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        movies.count
+    }
+
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "cell", for: indexPath)
+        cell.textLabel?.text = movies[indexPath.row].title
+        return cell
+    }
+
+
 }
